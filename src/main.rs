ecdebaad5281d71bce6fd09e7c3edf771d4407d0
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2018-04-25 23:52:06 +0100

    Add execution of scripts (Unix-only)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,50 @@
 extern crate serde_json;
 #[macro_use] extern crate serde_derive;
-extern crate termion;
 
 use std::env;
 use std::path::PathBuf;
-use termion::{color, style};
+use std::io::{stdin, BufRead};
+use std::process::Command;
+use std::os::unix::process::CommandExt;
 
 mod platforms;
-use platforms::{ScriptSource, ScriptList};
+use platforms::{ScriptSource, ScriptList, Script};
 use platforms::npm::Npm;
 
 struct ScriptGroup {
     name: String,
     scripts: ScriptList,
+}
+
+#[derive(PartialEq)]
+enum Step {
+    ChoosingGroup,
+    ChoosingScript,
+}
+
+fn clear() {
+    // println!("{}", clear::All);
+}
+
+fn print_groups(groups: &Vec<ScriptGroup>) {
+    for (index, group) in groups.iter().enumerate() {
+        println!("[{}]: {}", index, group.name);
+    }
+}
+
+fn print_scripts(scripts: &ScriptList) {
+    for (index, script) in scripts.iter().enumerate() {
+        println!("[{}]: {} ({})", index, script.name, script.command);
+    }
+}
+
+fn run_script(script: &Script) -> std::io::Error {
+    println!("Running script `{}`: `{}`", script.name, script.command);
+
+    // At this point, if successful, the script will be run and sl will be
+    // safely terminated. An error is returned and sl continues running if the
+    // exec fails for any reason
+    return Command::new("sh").arg("-c").arg(&script.command).exec();
 }
 
 fn main() {
@@ -24,18 +56,52 @@ fn main() {
         None => env::current_dir().unwrap(),
     };
 
-    let mut scripts: Vec<ScriptGroup> = Vec::new();
+    let mut groups: Vec<ScriptGroup> = Vec::new();
 
     let npm_scripts = Npm::new(path).get_scripts();
     if npm_scripts.is_ok() {
-        scripts.push(ScriptGroup{ name: String::from("npm"), scripts: npm_scripts.unwrap() });
+        groups.push(ScriptGroup { name: String::from("npm"), scripts: npm_scripts.unwrap() });
     }
 
-    for group in scripts {
-        println!("{}{}{}", color::Fg(color::Red), group.name, style::Reset);
+    let stdin = stdin();
+    let mut evt = stdin.lock().lines();
+    let mut step: Step = Step::ChoosingGroup;
+    let mut group_index: usize = 0;
 
-        for script in group.scripts {
-            println!("{}: {}", script.name, script.command);
-        }
+    loop {
+        clear();
+
+        match step {
+            Step::ChoosingGroup => { print_groups(&groups); },
+            Step::ChoosingScript => { print_scripts(&groups[group_index].scripts); },
+        };
+
+        let input: Result<i16, _> = evt.next().unwrap().unwrap().parse();
+
+        // If we were unable to parse an integer from the user input, then retry
+        if input.is_err() { continue; }
+
+        let index = input.unwrap();
+
+        match step {
+            Step::ChoosingGroup => {
+                if index < 0 || index >= groups.len() as i16 { continue; }
+                else {
+                    group_index = index as usize;
+                    step = Step::ChoosingScript;
+                    continue;
+                }
+            },
+            Step::ChoosingScript => {
+                if index < 0 || index >= groups[group_index].scripts.len() as i16 { continue; }
+                else {
+                    let selected_script = &groups[group_index].scripts[index as usize];
+
+                    run_script(selected_script);
+
+                    break;
+                }
+            },
+        };
     }
 }
